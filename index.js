8a42294a5fbd32e1401d38790e828f3b8b35f2d7
unknown <muktinathrajbanshi786@gmail.com>
2026-04-11 21:50:06 +0545

    aja yati la

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Typing Effect
 const text = "Hi, I'm Mukti 👋";
 let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 80);
+  }
+}
+
+typing();
